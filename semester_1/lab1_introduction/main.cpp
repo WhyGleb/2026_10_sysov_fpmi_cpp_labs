7bd5f2866990b4a2cb7cc0201140f99442209273
WhyGleb <sysovgleb@gmail.com>
2026-09-17 19:01:24 +0300

    1-е задание

--- semester_1/lab1_introduction/main.cpp
+++ semester_1/lab1_introduction/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <clocale>
 
-int main() {
-    std::cout << "Hello, Git! " << std::endl;
-    return 0;
+int main(){
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+    int a, b;
+    using std:: cout;
+    using std:: cin;
+    cout << "Введите числа ";
+    cin >> a >> b;
+    cout<<"\nРезультат: ";
+    if(a>=b){
+        for(int i = 1;b >= i;i++){
+            if((a%i==0)&& (b%i==0))
+            cout << i<< " ";
+        }
+    }
+    else{
+        for(int i = 1;a >=i ; i++ ){
+            if((a%i==0)&& (b%i==0))
+            cout << i<< " ";
+        }
+
+    }
+system("pause");
+return 0;
 }
+
+
